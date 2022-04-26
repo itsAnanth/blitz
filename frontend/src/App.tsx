37fd4e92b7bba261ee51220013cf356e23f4ab3e
EJBEAN2op <danonymousarmy@gmail.com>
2022-04-26 23:50:36 +0530

    feat(`app:Login`): login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LogIn from './components/LogIn';
+
+const style = {
+	wrapper: `bg-[#18191a] min-h-screen duration-[0.5s]`,
+	homeWrapper: `flex`,
+	center: `flex-1`,
+	main: `flex-1 flex justify-center`,
+	signupContainer: `flex items-center justify-center w-screen h-[70vh]`,
+}
+
 
 function App() {
+
+	const [name, setName] = useState('');
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [url, setUrl] = useState('');
+
 	return (
-		<div className="App">
-			HelloWorld!
+		<div className={style.wrapper}>
+
+			{loggedIn ? (
+				<div className={style.homeWrapper}>
+
+				</div>
+			) : (
+				<div className={style.signupContainer}>
+					<LogIn
+						setLoggedIn={setLoggedIn}
+						name={name}
+						setName={setName}
+						url={url}
+						setUrl={setUrl}
+					/>
+				</div>
+			)}
 		</div>
 	);
 }
