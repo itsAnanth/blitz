d70fcb0fed6182dc641bd830006aab252de156f1
EJBEAN2op <danonymousarmy@gmail.com>
2022-04-28 16:16:49 +0530

    test(`wsm`);

--- frontend/src/pages/Main.svelte
+++ frontend/src/pages/Main.svelte
@@ -1,3 +1,18 @@
-{#if true}
+<script lang="ts">
+    import WsManager from "../structures/WsManager";
+
+    let wsConnected = false;
+    const wsm = new WsManager();
+
+    wsm.addEventListener('connect', () => wsConnected = true);
+    wsm.connect();
+
+</script>
+
+
+{#if !wsConnected}
     <div>hello world</div>
+{:else}
+    <div>connected</div>
 {/if}
+
