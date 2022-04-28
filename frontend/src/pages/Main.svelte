0930dbdd29439123977b3c1757abc92abb29bc69
EJBEAN2op <danonymousarmy@gmail.com>
2022-04-29 01:30:12 +0530

    feat(`wsm`): register username in server

--- frontend/src/pages/Main.svelte
+++ frontend/src/pages/Main.svelte
@@ -2,6 +2,7 @@
     import Chat from './Chat.svelte';
     import LogIn from './LogIn.svelte';
     import WsManager from "../structures/WsManager";
+    import Message from '../../../shared/structures/Message';
 
     let wsConnected: boolean = false, loggedIn: boolean = false, username: string;
     const wsm = new WsManager();
@@ -9,11 +10,13 @@
     wsm.addEventListener('connect', () => {
         wsConnected = true;
     });
+
     wsm.connect();
 
     function handleLogin(ev: any) {
         loggedIn = true;
         username = ev.detail.username;
+        wsm.send(new Message({ type: Message.types.JOIN, data: [username] }));
     }
 
 </script>
