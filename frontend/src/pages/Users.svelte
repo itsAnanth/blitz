35e9f4a38890c5a57ca699b2e70ac96b2768919d
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-20 20:26:07 +0530

    feat: Users list component

---
+++ frontend/src/pages/Users.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+    export let data: { id: string; username: string; avatar: number };
+
+</script>
+
+<div class="user">
+    <img src="https://avatars.dicebear.com/api/adventurer-neutral/{data.avatar}.svg" alt="avatar" class="avatar">
+    <p class="username">{data.username}</p>
+</div>
