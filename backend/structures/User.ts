1872d6ddd35f9b83bf5fce38427d5826d8f49f84
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-25 13:46:30 +0530

    chore: use object parameters

--- backend/structures/User.ts
+++ backend/structures/User.ts
@@ -1,20 +1,21 @@
 import type { WebSocket } from 'uWebSockets.js';
+import { JsonWebKey } from 'node:crypto';
 
 class User {
-    name: string;
+    username: string;
     ws: WebSocket;
     avatar: number;
     publicKey: JsonWebKey;
 
-    constructor(name: string, ws: WebSocket, avatar: number, publicKey: JsonWebKey) {
-        this.name = name;
+    constructor({ username, ws, avatar, publicKey }: { username: string, ws: WebSocket, avatar: number, publicKey: JsonWebKey }) {
+        this.username = username;
         this.ws = ws;
         this.avatar = avatar;
         this.publicKey = publicKey;
     }
 
     serialize() {
-        return { username: this.name, avatar: this.avatar, publicKey: this.publicKey };
+        return { username: this.username, avatar: this.avatar, publicKey: this.publicKey };
     }
 }
 
