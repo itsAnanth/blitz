ced5a7b92dcac27d3755381e401853ace9f43f18
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-25 19:08:24 +0530

    chore: update types for encryption

--- shared/types/ChatMessage.ts
+++ shared/types/ChatMessage.ts
@@ -1,11 +1,11 @@
 interface ChatMessage {
-    content: string;
+    content: Uint8Array;
     timestamp: number;
     author: string;
     id?: string;
     avatar: number;
 }
 
-type ChatMessagePayload = { author: string, content: string, timestamp?: number, id? :string, avatar: number }
+type ChatMessagePayload = { author: string, content: Uint8Array, timestamp?: number, id? :string, avatar: number }
 
 export type { ChatMessage as IChatMessage, ChatMessagePayload };
