5a9500f037df0f94b52700f89cd3aaf42d30d7f4
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-26 15:23:48 +0530

    feat: svelte store typings

--- frontend/src/structures/Store.ts
+++ frontend/src/structures/Store.ts
@@ -1,15 +1,28 @@
 import { writable } from 'svelte/store';
-const messages = writable([]);
-const users = writable([]);
-const keys = writable({});
+import ChatMessage from '../../../shared/structures/ChatMessage';
+
+type Ikeys = {
+    privateKeyJwk: JsonWebKey | null;
+    publicKeyJwk: JsonWebKey | null;
+    sessionKey: JsonWebKey | null;
+    derivedKey: CryptoKey | null;
+    iv: Uint8Array | null;
+}
+
+type User = { id: string; username: string; avatar: number };
+
+const messages = writable<ChatMessage[]>([]);
+const users = writable<User[]>([]);
+const keys = writable<Ikeys>({
+    privateKeyJwk: null,
+    publicKeyJwk: null,
+    sessionKey: null,
+    derivedKey: null,
+    iv: null
+});
 
 export { messages, users, keys };
 
-type Ikeys = {
-    privateKeyJwk: JsonWebKey;
-    publickKeyJwk: JsonWebKey;
-    sessionKey: JsonWebKey;
-    derivedKey: CryptoKey;
-}
+
 
 export type { Ikeys };
