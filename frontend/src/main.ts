c88f9b43b1eb5f2d0be43d19cf996c6b39f33f21
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-26 15:21:53 +0530

    feat: dev env and logging

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,15 @@
 import App from './pages/Main.svelte';
+import Logger from '../../shared/structures/Logger';
+
+declare global {
+    interface Window {
+        DEV?: boolean;       
+    }
+}
+
+window.location.hostname === 'localhost' && (window.DEV = true);
+Logger.DEV = window.DEV ?? false;
+
 
 const app = new App({
     target: document.body
