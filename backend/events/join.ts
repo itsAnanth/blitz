4aef17dabeab8803880666cce2e6eefcdede949a
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-27 19:18:46 +0530

    chore: unified message id

--- backend/events/join.ts
+++ backend/events/join.ts
@@ -3,14 +3,14 @@ import WsServer from "../structures/WsServer";
 import Event from "../utils/Event";
 import User from "../structures/User";
 import Message from "../../shared/structures/Message";
-import crypto from 'crypto';
+import Utils from "../utils/Utils";
 
 export default new Event({
     type: MessageType.JOIN,
     callback(this: WsServer, ws, message) {
         console.log(`user joined, username -> ${message.data[0]} avatar -> ${message.data[1]}`)
         this.sockets.set(ws.id, new User({ username: message.data[0], ws, avatar: message.data[1], publicKey: message.data[2] }));
-        const data = { author: 'Blitz Bot', content: `${message.data[0]} joined the chat`, id: crypto.createHash('sha256').digest('hex') };
+        const data = { author: 'Blitz Bot', content: `${message.data[0]} joined the chat`, id: Utils.getMessageId() };
         this.app.publish('STATE/', Message.encode(new Message({ type: Message.types.JOIN, data: [data, this.usersData()] })), true);
         ws.send(Message.encode(new Message({ type: Message.types.SESSION, data: [this.session.sessionKey, this.session.iv] })), true);
     },
