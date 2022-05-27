3b5f20bbeb930d258d1f4082d02f85fca692ccdd
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-27 19:19:05 +0530

    chore: better naming scheme

--- backend/structures/User.ts
+++ backend/structures/User.ts
@@ -5,17 +5,17 @@ class User {
     username: string;
     ws: WebSocket;
     avatar: number;
-    publicKey: JsonWebKey;
+    publicKeyJwk: JsonWebKey;
 
     constructor({ username, ws, avatar, publicKey }: { username: string, ws: WebSocket, avatar: number, publicKey: JsonWebKey }) {
         this.username = username;
         this.ws = ws;
         this.avatar = avatar;
-        this.publicKey = publicKey;
+        this.publicKeyJwk = publicKey;
     }
 
     serialize() {
-        return { username: this.username, avatar: this.avatar, publicKey: this.publicKey };
+        return { username: this.username, avatar: this.avatar, publicKeyJwk: this.publicKeyJwk };
     }
 }
 
