0bee0f1a2c73c715f5114eb54d59e9b61e4132e1
EJBEAN2op <danonymousarmy@gmail.com>
2022-07-17 11:18:33 +0530

    refactor(`server:leave`)
    - type assertion
    - unified coding standards

--- backend/events/leave.ts
+++ backend/events/leave.ts
@@ -3,12 +3,27 @@ import WsServer from "../structures/WsServer";
 import Event from "../utils/Event";
 import Message from "../../shared/structures/Message";
 import Utils from "../utils/Utils";
+import ChatMessage from "../../shared/structures/ChatMessage";
+import { Updates } from "../../shared/types/Updates";
 
 export default new Event({
     type: MessageType.LEAVE,
     callback(this: WsServer, ws, message) {
         this.sockets.delete(ws.id);
-        const data = { author: 'Blitz Bot', content: `${message.data[0]} left the chat`, id: Utils.getMessageId() };
-        this.app.publish('STATE/', Message.encode(new Message({ type: Message.types.LEAVE, data: [data, this.usersData()] })), true);
+
+        const data = new ChatMessage({
+            author: 'Blitz Bot',
+            content: `${message.data[0]} left the chat`,
+            id: Utils.getMessageId(),
+            timestamp: Date.now(),
+            avatar: 2
+        }).serialize();
+
+        const msgData: Updates.Server.LEAVE = [data, this.usersData()];
+
+        this.app.publish('STATE/', new Message({
+            type: Message.types.LEAVE,
+            data: msgData
+        }).encode(), true);
     },
 })
