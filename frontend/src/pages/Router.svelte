9d6a9d7777a79cbef1507b0ef34a9f0f78c2c18b
EJBEAN2op <danonymousarmy@gmail.com>
2022-07-31 16:57:51 +0530

    feat(`client:routing`)

---
+++ frontend/src/pages/Router.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Router, Route, Link } from "svelte-navigator";
+    import Chat from './Chat.svelte';
+    import SignIn from "./SignIn.svelte";
+    import SignUp from "./SignUp.svelte";
+</script>
+
+<Router>
+    <Route path="/" component={SignUp} />
+    <Route path="signin" component={SignIn} />
+    <Route path="chat" component={Chat} />
+</Router>
+
+
